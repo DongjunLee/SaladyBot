8c5922fb4afe0cc4cc3e2c716215c906198ed678
DongjunLee <ehdwns2045@gmail.com>
2016-08-15 02:03:55 +0900

    First Commit - lins05/slackbot simple test

---
+++ bot.py
@@ -0,0 +1,10 @@
+from slackbot.bot import Bot
+
+def main():
+	bot = Bot()
+	bot.run()
+
+	gitbut()
+
+if __name__ == "__main__":
+	main()
---
+++ slackbot_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+API_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+ERRORS_TO = 'dongjun'
+
+PLUGINS = [
+	'simple_plugin'
+]
