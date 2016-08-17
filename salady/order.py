0b51272e3e1cea39746bd9536a5f3c8b9f57b65a
dongjun_lee <djlee@konolabs.net>
2016-08-17 20:08:08 +0900

    Modify: first item -> salady name

--- salady/order.py
+++ salady/order.py
@@ -1,10 +1,11 @@
-#coding: UTF-8
+# -*- coding: utf-8 -*- 
 
 import datetime
 import json
 import os
 
 from salady.menu import SaladyMenu
+from salady.translate import SaladyTranslator
 
 class SaladyOrder(object):
 
@@ -13,18 +14,24 @@ class SaladyOrder(object):
 
 	def order(self, item_list):
 		menu = SaladyMenu()
+		translator = SaladyTranslator() 
+
+		salady_kor = item_list[0].strip()
+		salady_eng = translator.lang_ko2en(salady_kor)
+		del item_list[0]
+		main, sub, dressing, price = menu.get_salady(salady_eng) 
 
 		main_topping_kor = []
-		main_topping_eng = []
+		main_topping_eng = main
 		sub_topping_kor = []
-		sub_topping_eng = []
+		sub_topping_eng = sub
 
 		dressing_kor = ""
-		dressing_eng = ""
+		dressing_eng = dressing
 		size_up = False
 
 		for item_kor in item_list:
-			item_eng = menu.lang_ko2en(item_kor.strip())
+			item_eng = translator.lang_ko2en(item_kor.strip())
 			category = menu.item_2_category(item_eng)
 			if category == "main":
 				main_topping_kor.append(item_kor)
@@ -48,15 +55,15 @@ class SaladyOrder(object):
 		else:
 			size_up_str = "X"
 
-		price = self.calculate_total_price(main_topping_eng, sub_topping_eng,
+		price += self.calculate_total_price(main_topping_eng, sub_topping_eng,
 											dressing_eng, size_up)
 		self.append_order_list(main_topping_eng, sub_topping_eng, dressing_eng, size_up)
 			
-		return main_topping_kor, sub_topping_kor, dressing_kor, size_up_str, price
+		return salady_kor, main_topping_kor, sub_topping_kor, dressing_kor, size_up_str, price
 
 	def calculate_total_price(self, main_topping, sub_topping, dressing, size_up):
-		price = 4100 # Basic
 		menu = SaladyMenu()
+		price = 0
 
 		for main in main_topping:
 			price += int(menu.get_price(main))
